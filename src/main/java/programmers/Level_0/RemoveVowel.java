b2646e6d8d5666dde7655e5f832ce15cc28e2d1f
hazel <waithjno@gmail.com>
2022-12-22 17:17:41 +0900

    added ArraySimilarity, RemoveVowel

---
+++ src/main/java/programmers/Level_0/RemoveVowel.java
@@ -0,0 +1,38 @@
+package programmers.Level_0;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * @author hazel
+ */
+public class RemoveVowel {
+
+    public static String solution(String my_string) {
+        StringBuilder answer = new StringBuilder();
+
+        int cnt = 0;
+        String[] arr1 = my_string.split("");
+        //[n, i, c, e,  , t, o,  , m, e, e, t,  , y, o, u] = i
+        String[] arr2 = {"a", "e", "i","o", "u"};  //j
+
+        for(int i =0; i < arr1.length; i ++){
+            for(int j = 0;  j <arr2.length; j++){
+                if(!Objects.equals(arr1[i], arr2[j])){
+                    cnt +=1;
+                    if(cnt == 5){
+                        answer.append(arr1[i]);
+                        cnt =0;
+                    }
+                }
+            }
+        }
+        return answer.toString();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(solution("nice to meet you"));
+
+    }
+}
