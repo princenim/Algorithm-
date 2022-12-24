66a5f82e8259ff052a2986b8e54de8e97f383da6
hazel <waithjno@gmail.com>
2022-12-24 17:07:12 +0900

    added level0

--- src/main/java/programmers/Level_0/Pizza2.java
+++ src/main/java/programmers/Level_0/Pizza2.java
@@ -2,15 +2,21 @@ package programmers.Level_0;
 
 public class Pizza2 {
     public static int solution(int n) {
-        int answer = 0;
-        return answer;
+        int pizzaCnt =1;
+        if(n > 7) {
+            pizzaCnt = n / 7;
+            if (n % 7 != 0) {
+                pizzaCnt += 1;
+            }
+        }
+
+
+        return pizzaCnt;
 
     }
 
     public static void main(String[] args) {
-        //test test
-        //test22222
-        System.out.println(solution(1));
+        System.out.println(solution(100));
 
     }
 }
