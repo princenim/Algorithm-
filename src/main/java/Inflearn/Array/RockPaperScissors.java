959a7ade0a7a8a2486217259e0140ea731fd773c
hazel <waithjno@gmail.com>
2023-02-23 19:07:57 +0900

    solved inflearn (RockPaperScissors)

--- src/main/java/Inflearn/Array/RockPaperScissors.java
+++ src/main/java/Inflearn/Array/RockPaperScissors.java
@@ -1,21 +1,32 @@
 package Inflearn.Array;
 
-import java.time.Period;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 /**
  * @author hazel
  */
 public class RockPaperScissors {
-    public static ArrayList<Character> solution(int n, ArrayList<Integer> arr1, ArrayList<Integer> arr2){
-        ArrayList<Character> answer=  new ArrayList<>();
+    public static ArrayList<Character> solution(int n, ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
+        ArrayList<Character> answer = new ArrayList<>();
 
+        for (int i = 0; i < n; i++) {
+            if (Objects.equals(arr1.get(i), arr2.get(i))) {
+                answer.add('D');
+            } else if (arr1.get(i) == 1 && (arr2.get(i) == 3)) {
+                answer.add('A');
+            } else if (arr1.get(i) == 2 && arr2.get(i) == 1) {
+                answer.add('A');
+            } else if (arr1.get(i) == 3 && arr2.get(i) == 2) {
+                answer.add('A');
+            } else {
+                answer.add('B');
+            }
 
+        }
 
-
-
-
+        System.out.println(answer);
         return answer;
     }
 
@@ -31,7 +42,10 @@ public class RockPaperScissors {
             arr2.add(in.nextInt());
         }
 
+        //출력
+        for (char x : solution(n, arr1, arr2)) {
+            System.out.println(x);
+        }
 
-        System.out.println(solution(n, arr1, arr2));
     }
 }
