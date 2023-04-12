35e02afdd3a7d9d5f2ca9fc7a3fafc5c049669c9
hazel <waithjno@gmail.com>
2023-04-13 01:22:59 +0900

    solverd inflearn (recursive(memoization))

--- src/main/java/Inflearn/Recursive/Fibonacci1.java
+++ src/main/java/Inflearn/Recursive/Fibonacci1.java
@@ -1,17 +1,13 @@
 package Inflearn.Recursive;
 
-import com.sun.tools.javac.Main;
-
-import java.util.Date;
 import java.util.Scanner;
 
 /**
  * @author hazel
  */
-public class Fibonacci {
+public class Fibonacci1 {
     //7-4.피보나치 수열 : 앞의 2개의 수를 합해 다음 숫자가 되는 숫자
-
-    //방법1
+    //방법1 - 하나씩 계속 돌기때문에 비효율적
     public static int DFS(int n) {
         if (n == 1) {
             return 1;
