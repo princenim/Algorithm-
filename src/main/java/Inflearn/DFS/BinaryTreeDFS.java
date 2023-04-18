c0f1ae1552dd6f35d545756267a6140da31fed32
hazel <waithjno@gmail.com>
2023-04-18 15:30:13 +0900

    solved inflearn (BinaryTreeBFS, subset)

--- src/main/java/Inflearn/DFS/BinaryTreeDFS.java
+++ src/main/java/Inflearn/DFS/BinaryTreeDFS.java
@@ -8,7 +8,8 @@ package Inflearn.DFS;
 class Node {
     //데이터 값
     int data;
-    //왼, 오 자식의 노드 주소
+    //왼,  오 자식의 노 드주소
+    //node 라는 클래스의 객체 주소를 저장
     Node lt, rt;
 
     //생성자
@@ -21,32 +22,30 @@ class Node {
 public class BinaryTreeDFS {
     Node root;
 
-    //7-5. 이진트리순회 (Depth-First Search)
+    //7-5. 이진트리순회 (Depth-First Search) - 재귀를 활용
     public void DFS(Node root) {
         //DFSMain의 인스턴스 메서드
         if (root == null) {
             //맨 밑 노드
             return;
         } else {
-
             //전위순회 출력
-            //System.out.print(root.data + " ");
+            System.out.print(root.data + " ");
             DFS(root.lt);
             //중위순회 출력
-            //System.out.print(root.data + " ");
+            System.out.print(root.data + " ");
             DFS(root.rt);
             //후위순회 출력
-            //System.out.print(root.data + " ");
-
+            System.out.print(root.data + " ");
         }
     }
 
     public static void main(String[] args) {
-        // DFSMain class 객체, 그리고 이 안의 객체안에 root라는 변수가 존재
+        // DFSMain class 객체, 그리고 이 안의 객체안에 root라는 변수가 존재 (node라는 클래스의 객체 주소를 저장하고 있는 변수)
         BinaryTreeDFS tree = new BinaryTreeDFS();
 
         //이진 트리 만들기
-        //new로 객체를 생성하고 객체끼리 주소값으로 연결하기
+        //new로 객체를 생성하고 생긴 주소값을 tree.root랑 연결
         tree.root = new Node(1);
         tree.root.lt = new Node(2);
         tree.root.rt = new Node(3);
--- src/main/java/Inflearn/DFS/Subset.java
+++ src/main/java/Inflearn/DFS/Subset.java
@@ -5,18 +5,18 @@ package Inflearn.DFS;
  * @author hazel
  */
 public class Subset {
-    //7-6. 부분 집합 구하기
+    //7-6. 부분 집합 구하기 (경우의 수를 모두 구하기 ! )
     //자연수 n이 주어지면 1부터 n까지의 원소를 갖는 집합의 부분 집합을 모두 출력해라 -> 2의 n 제곱
-
     static int n;
     static int[] ch;
 
     // L은 숫자
     public void DFS(int L) {
         if (L == n + 1) {
-            //종착점에 오면
-            //값이 1인 ch의 배열 값을 출력
+            //종착점에 오면 값이 1인 ch의 배열 값을 출력
+            //n이 3이니까 4가 된다면 멈추기
             String tmp = "";
+            //0은 없다고 출력 X 하고 값이 1인 인덱스만 출력
             for (int i = 1; i <= n; i++) {
                 if (ch[i] == 1) {
                     tmp += (i + " ");
@@ -35,11 +35,15 @@ public class Subset {
         }
     }
 
-
     public static void main(String[] args) {
         Subset subset = new Subset();
         n = 3;
+
+        //index 번호가 출력할 배열의 값
         ch = new int[n + 1];
+
+
+        //1부터 시작해서 숫자를 출력
         subset.DFS(1);
     }
 }
