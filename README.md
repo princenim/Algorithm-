1312495d8ed8b2bab41402d32eec8d5adf020e97
hazel <waithjno@gmail.com>
2023-09-20 10:55:37 +0900

    .gitignore 수정

--- README.md
+++
@@ -1,5 +0,0 @@
-# Algorithm
-알고리즘 풀이 연습 기록
-
-- Programmers
-- Inflearn
--- src/main/java/Algorithm.java
+++
@@ -1,8 +0,0 @@
-public class Algorithm {
-    public static void main(String[] args) {
-        System.out.println("this is project for algo");
-
-        //test
-        //test
-    }
-}
