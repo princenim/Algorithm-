8ab8990fc59aacf8dc1b38ff07a7d7c92b60fc12
hazel <waithjno@gmail.com>
2023-12-17 22:18:20 +0900

    solved BOJ 10998,18108,10926

---
+++ src/main/BOJ/BOJ10926.java
@@ -0,0 +1,16 @@
+package main.BOJ;
+
+import java.util.Scanner;
+
+/**
+ * @author hazel
+ */
+public class BOJ10926 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(str + "??!");
+
+    }
+}
