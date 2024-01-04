15e0883651a10fd5d9c5c44c62f9547034a88d83
hazel <waithjno@gmail.com>
2024-01-04 10:17:27 +0900

    solved BOJ 10950

---
+++ src/main/BOJ/BOJ10950.java
@@ -0,0 +1,20 @@
+package main.BOJ;
+
+import java.util.Scanner;
+
+/**
+ * @author hazel
+ */
+public class BOJ10950 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a + b);
+        }
+
+    }
+}
