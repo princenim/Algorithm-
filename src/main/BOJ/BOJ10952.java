ef3e71ca28c5182fcc0b056070394c1bac56f989
hazel <waithjno@gmail.com>
2024-01-12 09:52:25 +0900

    update BOJ 10952

---
+++ src/main/BOJ/BOJ10952.java
@@ -0,0 +1,21 @@
+package main.BOJ;
+
+import java.util.Scanner;
+
+/**
+ * @author hazel
+ */
+public class BOJ10952 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            if(a == 0 && b ==0){
+                break;
+            }
+            System.out.println(a +b);
+        }
+    }
+}
