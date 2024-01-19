26c50e27b3caa95e4857a6bc280592799a404c9f
hazel <waithjno@gmail.com>
2024-01-19 15:09:04 +0900

    solve BOJ 10809

---
+++ src/main/BOJ/BOJ10809.java
@@ -0,0 +1,25 @@
+package main.BOJ;
+
+import java.util.Scanner;
+
+/**
+ * @author hazel
+ */
+public class BOJ10809 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+
+        String str = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            System.out.print(next.indexOf(ch) + " ");
+
+
+        }
+
+    }
+
+
+}
