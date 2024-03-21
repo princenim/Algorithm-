78f77c2849517fad0c53ac710b424cbface09655
hazel <waithjno@gmail.com>
2024-03-21 19:27:16 +0900

    solve BOJ 11722 - 가장 긴 감소하는 부분 수열

--- src/main/BOJ/BOJ/BOJ11722.java
+++ src/main/BOJ/BOJ/BOJ11722.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- * 가장 긴 감소하는 부분 수열 https://www.acmicpc.net/problem/11722 DP
+ * 가장 긴 감소하는 부분 수열, https://www.acmicpc.net/problem/11722, DP
  *
  * @author hazel
  */
