3b0685005ca09a000c1f674670454d84a93ee2c7
hazel <waithjno@gmail.com>
2024-06-16 21:10:27 +0900

    update BOJ 2178 - 미로 탐색

--- src/main/BOJ/BOJ/BOJ2178.java
+++ src/main/BOJ/BOJ/BOJ2178.java
@@ -5,79 +5,76 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
+
 
 /**
- * 미로탐색
- * BFS 최단거리구하기
+ * 미로 탐색, https://www.acmicpc.net/problem/2178, DFS/BFS
+ *
  * @author hazel
  */
 public class BOJ2178 {
 
-    static int[] dx = {0, -1, 0, 1}; //좌상우하
-    static int[] dy = {-1, 0, 1, 0};
-
-    static int N;
-    static int M;
-
     static int[][] arr;
     static boolean[][] visited;
-    static int cnt = 1;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int n, m;
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] nm = br.readLine().split(" ");
-        N = Integer.parseInt(nm[0]); //행
-        M = Integer.parseInt(nm[1]); //열
-        arr = new int[N][M];
-        visited = new boolean[N][M];
-
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = str.charAt(j) - '0';
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+        visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = s.charAt(j) - '0';
             }
         }
 
-
-        //System.out.println(Arrays.deepToString(arr));
         bfs(0, 0);
         //System.out.println(Arrays.deepToString(arr));
-        System.out.println(arr[N - 1][M - 1]);
-
-
+        System.out.println(arr[n - 1][m - 1]);
     }
 
     public static void bfs(int x, int y) {
-
-        Queue<Pair> q = new LinkedList<>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(x, y));
         visited[x][y] = true;
-        q.add(new Pair(x, y));
 
         while (!q.isEmpty()) {
-            Pair current = q.poll();
-            
+            Node current = q.poll();
+
             for (int i = 0; i < 4; i++) {
                 int nx = current.x + dx[i];
                 int ny = current.y + dy[i];
 
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+                //범위 체크
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
                     continue;
                 }
 
-                if (arr[nx][ny] == 1 && !visited[nx][ny]) {
-                    q.add(new Pair(nx, ny));
-                    visited[nx][ny] = true;
-                    arr[nx][ny] = (arr[current.x][current.y] + 1);
+                if (!visited[nx][ny]) {
+                    if (arr[nx][ny] == 1) {
+                        q.add(new Node(nx, ny));
+                        visited[nx][ny] = true;
+                        arr[nx][ny] = arr[current.x][current.y] + 1;
+                    }
                 }
-
             }
         }
     }
-    static class Pair {
-        int x, y;
 
-        Pair(int x, int y) {
+    static class Node {
+
+        int x;
+        int y;
+
+        Node(int x, int y) {
             this.x = x;
             this.y = y;
         }
